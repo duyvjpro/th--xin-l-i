0575b9ea28df276f7adb0d6fe1620fdb23954afd
duyvjpro <95209512+duyvjpro@users.noreply.github.com>
2021-11-29 14:46:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
-
+const contentCard = xin lỗi  bạn vanh nhé của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau'
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
