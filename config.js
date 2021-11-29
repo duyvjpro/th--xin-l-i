d24178641e87a9e36d31fa796886a5169d0d10d8
duyvjpro <95209512+duyvjpro@users.noreply.github.com>
2021-11-29 14:53:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-const nameGirl = 'Em';
+const nameGirl = 'Vanh';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = xin lỗi  bạn vanh nhé của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau'
+const eventName = 'Thư xin lỗi';
+const titleCard = 'Gửi đến Vanh';
+const contentCard = xin lỗi bạn Vanh nhé xin lỗi vì sự nóng giận của tôi . Chân thành xin lỗi bạn'
+
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
